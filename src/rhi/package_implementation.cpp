6a5eb4f0822a1fcf7aa3de7ed3c8526ddb11a262
Keshav Verma <vkeshav300@gmail.com>
2025-09-04 21:34:34 -0700

    RHI: add '#ifdef __APPLE__' to package implementation

--- src/rhi/package_implementation.cpp
+++ src/rhi/package_implementation.cpp
@@ -1,6 +1,10 @@
+#ifdef __APPLE__
+
 #define NS_PRIVATE_IMPLEMENTATION
 #define CA_PRIVATE_IMPLEMENTATION
 #define MTL_PRIVATE_IMPLEMENTATION
 #include <Foundation/Foundation.hpp>
 #include <Metal/Metal.hpp>
 #include <QuartzCore/QuartzCore.hpp>
+
+#endif
