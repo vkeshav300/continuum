a17f345a362cdab43fdee7fd77b32ef0e8217a5e
Keshav Verma <vkeshav300@gmail.com>
2025-10-29 18:46:18 -0700

    [RHI] merge window with renderer

--- include/rhi/renderer.hpp
+++ include/rhi/renderer.hpp
@@ -1,33 +1,55 @@
 #pragma once
 
-#include "window.hpp"
+#include "../scene.hpp"
 
 #include <cstdint>
 
 #ifdef __APPLE__
 
+#include <AppKit/AppKit.hpp>
+#include <GLFW/glfw3.h>
 #include <Metal/Metal.hpp>
+#include <QuartzCore/QuartzCore.hpp>
 
 namespace CTNM::RHI {
 
 class Renderer {
 private:
+  /* Window interface */
+  GLFWwindow *m_window;
+  NS::Window *m_ns_window;
+
+  /* Metal rendering */
+  CA::MetalLayer *m_layer;
+  CA::MetalDrawable *m_drawable;
+
   MTL::Device *m_device;
   MTL::Library *m_library;
   MTL::CommandQueue *m_cmd_queue;
   MTL::CommandBuffer *m_cmd_buffer;
 
-  Window *m_window;
+  static void error_callback(const int code, const char *description);
+
+  static void framebuffer_size_callback(GLFWwindow *window, const int width,
+                                        const int height);
+
+  void resize_framebuffer(const int width, const int height);
 
 public:
-  Renderer(const uint16_t width, const uint16_t height);
+  Renderer(const uint16_t &width, const uint16_t &height);
   ~Renderer();
 
+  bool should_close() const;
+  void poll_events() const;
+
+  void next_drawable();
+  bool drawable();
+
   void stage();
-  void render();
+  void render_current_drawable(const Scene &scene);
+  void render_to_preview(const Scene &scene);
 };
 
 } // namespace CTNM::RHI
 
 #endif
-
