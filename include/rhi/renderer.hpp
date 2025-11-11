032dda46ed1726b9b732553da03aeb6894e0a66a
Keshav Verma <vkeshav300@gmail.com>
2025-11-10 19:43:39 -0800

    Revert "Revert "Ecs""

--- include/rhi/renderer.hpp
+++ include/rhi/renderer.hpp
@@ -1,31 +1,55 @@
 #pragma once
 
-#include "window.hpp"
+#include <cstdint>
+
+#include <entt/entt.hpp>
 
 #ifdef __APPLE__
 
+#include <AppKit/AppKit.hpp>
+#include <GLFW/glfw3.h>
 #include <Metal/Metal.hpp>
+#include <QuartzCore/QuartzCore.hpp>
 
 namespace CTNM::RHI {
 
 class Renderer {
 private:
+  /* Window interface */
+  GLFWwindow *m_window;
+  NS::Window *m_ns_window;
+
+  /* Metal rendering */
+  CA::MetalLayer *m_layer;
+  CA::MetalDrawable *m_drawable;
+
   MTL::Device *m_device;
   MTL::Library *m_library;
   MTL::CommandQueue *m_cmd_queue;
   MTL::CommandBuffer *m_cmd_buffer;
 
-  Window *m_window;
+  static void error_callback(const int code, const char *description);
+
+  static void framebuffer_size_callback(GLFWwindow *window, const int width,
+                                        const int height);
+
+  void resize_framebuffer(const int width, const int height);
 
 public:
-  Renderer();
+  Renderer(const uint16_t &width, const uint16_t &height);
   ~Renderer();
 
-  void stage();
-  void render();
+  bool should_close() const;
+  void poll_events() const;
+
+  void next_drawable();
+  bool drawable();
+
+  void stage(entt::registry &registry);
+  void render_current_drawable(entt::registry &registry);
+  void render_to_preview(entt::registry &registry);
 };
 
 } // namespace CTNM::RHI
 
 #endif
-
