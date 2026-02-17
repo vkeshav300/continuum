086b8832e79aa3bb9af9acce6d90cab985f55ae6
Keshav Verma <vkeshav300@gmail.com>
2026-02-17 15:44:00 -0800

    [Simulation] add default values to components

--- include/components.hpp
+++ include/components.hpp
@@ -5,19 +5,19 @@
 namespace CTNM::Components {
 
 struct Transform {
-  vec_f3 pos; // Position
-  vec_f3 scl; // Scale
-  vec_f4 rtn; // Rotation
+  vec_f3 pos = {0.0f, 0.0f, 0.0f};       // Position
+  vec_f3 scl = {1.0f, 1.0f, 1.0f};       // Scale
+  vec_f4 rtn = {0.0f, 0.0f, 0.0f, 0.0f}; // Rotation
 };
 
 struct Sphere_AABB {
-  float r;
+  float r = 1.0f;
 };
 
 struct Camera {
-  vec_f3 pos;
-  vec_f3 fpos; // Point to focus camera at
-  float fov;
+  vec_f3 pos = {-5.0f, 0.0f, 0.0f};
+  vec_f3 fpos = {0.0f, 0.0f, 0.0f}; // Point to focus camera at
+  float fov = 120.0f;
 };
 
 } // namespace CTNM::Components
